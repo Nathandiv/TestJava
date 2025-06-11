93022352767076d69e8e2589849176061fe3434b
Nathandiv <k.n.hlungwani@gmail.com>
2025-06-11 15:42:24 +0200

    added changes

--- src/main/java/com/mvp/todo/todoApp/model/Todo.java
+++ src/main/java/com/mvp/todo/todoApp/model/Todo.java
@@ -1,36 +1,69 @@
 package com.mvp.todo.todoApp.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.security.Timestamp;
+
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
+@Getter
+@Setter
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false,columnDefinition = "varchar",length = 100,updatable = false)
-     Long id;
+    @Column(nullable = false, columnDefinition = "varchar", length = 100, updatable = false)
+    Long id;
     @Column(nullable = false)
     String title;
     @Column(nullable = false)
     String description;
     @Column(nullable = false)
-      TodoStatus todoStatus;
+    TodoStatus todoStatus;
 
     @CreationTimestamp
     @Column(updatable = false)
     Timestamp timestamp;
     @UpdateTimestamp
     Timestamp updatedTimestamp;
+
+
+    public TodoStatus getTodoStatus() {
+        return todoStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTodoStatus(TodoStatus status) {
+        this.todoStatus = status;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
